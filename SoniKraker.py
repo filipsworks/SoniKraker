d8ea0e5c111e27920fba25d82ccc960a25f1eadb
filipsworks <47939098+filipsworks@users.noreply.github.com>
2023-06-24 00:00:31 +0200

    Update SoniKraker.py

--- SoniKraker.py
+++ SoniKraker.py
@@ -1,21 +1,26 @@
 def CRC16(crc, buffer):
-    for _ in range(len(buffer)):
-        crc ^= buffer.pop(0) << 8
-        bits = 0
-        while bits < 8:
-            if (crc & 0x8000) != 0:
-                crc = (2 * crc) ^ 0x1021
+    for i in range(len(buffer)):
+        crc ^= buffer[i] << 8
+        for _ in range(8):
+            if crc & 0x8000:
+                crc = (crc << 1) ^ 0x1021
             else:
-                crc *= 2
-            bits += 1
+                crc <<= 1
+        crc &= 0xFFFF
     return crc
 
 
-nfctag_uid = bytearray.fromhex(input("Input UID: "))
-nfc_mfg = input("Input MFG: ").encode('ascii')
+def int_to_colon_hex(integer):
+    hex_string = hex(integer)[2:]
+    hex_string = ":".join(hex_string[i:i+2] for i in range(0, len(hex_string), 2))
+    return hex_string
+
+
+nfctag_uid = bytearray.fromhex(input("Input UID: ").replace(":", ""))
+nfc_mfg = bytearray(input("Input MFG: ").encode('ascii'))
+seconds_used = "00:00:02:00" # 0 sec
 
 pwd = CRC16(0x49A3, nfctag_uid.copy())
 pwd = pwd | (CRC16(pwd, list(nfc_mfg)) << 16)
-pwd = (crc_calc >> 8) & 0x00FF00FF | (crc_calc << 8) & 0xFF00FF00
-
-print(f"NFC PWD: 0x{crc_calc:08X}")
+pwd = (pwd >> 8) & 0x00FF00FF | (pwd << 8) & 0xFF00FF00
+print(f"SoniKraker CMD: 1B:{int_to_colon_hex(pwd).upper()},A2:24:{seconds_used}")
