f517d32ccc75613528e18dad1e0bea14f4beb116
filipsworks <47939098+filipsworks@users.noreply.github.com>
2023-06-24 01:02:06 +0200

    added support for < 256 sec used and full renew default for Leave # sessions input

--- SoniKraker.py
+++ SoniKraker.py
@@ -11,7 +11,7 @@ def CRC16(crc, buffer):
 
 
 def int_to_colon_hex(integer, bytes_reversed=None):
-    hex_string = hex(integer)[2:]
+    hex_string = hex(integer)[2:].zfill(4)
     hex_string = ":".join(hex_string[i:i+2] for i in range(0, len(hex_string), 2))
     if bytes_reversed:
         hex_values = hex_string.split(":")
@@ -22,12 +22,12 @@ def int_to_colon_hex(integer, bytes_reversed=None):
 def tries_left_to_seconds_used(tries):
     initial_tries = 180
     maximum_seconds_used = 21600 # 60 sec * 2 minutes brushing * 2 times a day * 30 days in month * 3 months recommended lifetime
-    return 120*(180-int(tries))
+    return 0 if tries == "" else 120*(180-int(tries))
 
 
 nfctag_uid = bytearray.fromhex(input("Input UID: ").replace(":", ""))
 nfc_mfg = bytearray(input("Input MFG: ").encode('ascii'))
-time_left = int_to_colon_hex(tries_left_to_seconds_used(input("Leave # tries: ")), True)
+time_left = int_to_colon_hex(tries_left_to_seconds_used(input("Leave # sessions (or leave empty for 180): ")), True)
 seconds_used = f"{time_left}:02:00" # 0 sec
 
 pwd = CRC16(0x49A3, nfctag_uid.copy())
