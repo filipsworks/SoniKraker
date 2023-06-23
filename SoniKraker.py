9defc2b70243a38f6593c481266207926b1cfaf7
filipsworks <47939098+filipsworks@users.noreply.github.com>
2023-06-24 00:40:44 +0200

    Custom unfinished sessions input

--- SoniKraker.py
+++ SoniKraker.py
@@ -10,15 +10,25 @@ def CRC16(crc, buffer):
     return crc
 
 
-def int_to_colon_hex(integer):
+def int_to_colon_hex(integer, bytes_reversed=None):
     hex_string = hex(integer)[2:]
     hex_string = ":".join(hex_string[i:i+2] for i in range(0, len(hex_string), 2))
+    if bytes_reversed:
+        hex_values = hex_string.split(":")
+        reversed_hex_values = hex_values[::-1]
+        hex_string = ":".join(reversed_hex_values)
     return hex_string
+
+def tries_left_to_seconds_used(tries):
+    initial_tries = 180
+    maximum_seconds_used = 21600 # 60 sec * 2 minutes brushing * 2 times a day * 30 days in month * 3 months recommended lifetime
+    return 120*(180-int(tries))
 
 
 nfctag_uid = bytearray.fromhex(input("Input UID: ").replace(":", ""))
 nfc_mfg = bytearray(input("Input MFG: ").encode('ascii'))
-seconds_used = "00:00:02:00" # 0 sec
+time_left = int_to_colon_hex(tries_left_to_seconds_used(input("Leave # tries: ")), True)
+seconds_used = f"{time_left}:02:00" # 0 sec
 
 pwd = CRC16(0x49A3, nfctag_uid.copy())
 pwd = pwd | (CRC16(pwd, list(nfc_mfg)) << 16)
